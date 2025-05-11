16e3c5cebb8c82645953b2c3417d8b97c8ce0f41
Ye Yu <nearjilt@gmail.com>
2025-05-11 14:19:00 +0800

    feat: 实现 apply

---
+++ src/apply/index.js
@@ -0,0 +1,15 @@
+Function.prototype.yeApply = function (thisArg, argArray) {
+  const thisArg =
+    thisArg !== null && thisArg !== undefined ? Object(thisArg) : window;
+
+  const fn = Symbol();
+
+  thisArg[fn] = this;
+
+  // * 可以不传参数
+  const result = argArray ? thisArg[fn](...argArray) : thisArg[fn]();
+
+  delete thisArg[fn];
+
+  return result;
+};
